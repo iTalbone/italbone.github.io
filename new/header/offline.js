44d92c92197ca29eecf926c4da71eedbdf6818aa
Talbone <50276802+iTalbone@users.noreply.github.com>
2020-02-10 21:49:46 -0300

    header easter egg

---
+++ new/header/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1581382019,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/logo-sheet0.png",
+		"images/lettert-sheet0.png",
+		"images/lettera-sheet0.png",
+		"images/letterl-sheet0.png",
+		"images/letterb-sheet0.png",
+		"images/lettero-sheet0.png",
+		"images/lettern-sheet0.png",
+		"images/lettere-sheet0.png",
+		"images/border-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
